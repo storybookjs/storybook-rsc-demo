07f224afee80e8fa419284d427b1393e0e95de70
Kasper Peulen <kasperpeulen@gmail.com>
2024-05-03 11:32:36 +0200

    Add back deleted story

---
+++ components/note-list.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react'
+import NoteList from '#components/note-list'
+import { createNotes } from '#mocks/notes'
+
+const meta = {
+  component: NoteList,
+  args: {
+    searchText: null,
+  },
+} satisfies Meta<typeof NoteList>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Empty: Story = {
+  args: { notes: [] },
+}
+
+export const Default: Story = {
+  args: {
+    notes: createNotes(),
+  },
+}
