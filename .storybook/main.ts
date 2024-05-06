47dd84a6108e1eaba9a0c6a21d192c9477a6a13f
Yann Braga <yannbf@gmail.com>
2024-05-06 16:49:46 +0200

    add test coverage

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-coverage',
     '@chromatic-com/storybook',
   ],
   framework: {
