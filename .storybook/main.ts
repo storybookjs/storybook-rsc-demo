ce5aa7af9f90a48b6594533a0da71f2b9b8a03ee
Kasper Peulen <kasperpeulen@gmail.com>
2024-08-10 16:40:18 +0200

    Fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,9 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
+  features: {
+    experimentalRSC: true,
+  },
   staticDirs: ['../public'],
 }
 export default config
