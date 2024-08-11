d7cfad841a0adbc6d27133c5aa284db6a87c1b8f
Kasper Peulen <kasperpeulen@gmail.com>
2024-08-11 15:28:57 +0200

    Remove something that did not work

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -27,10 +27,6 @@ const preview: Preview = {
   },
   loaders: [mswLoader],
   beforeEach({ context, parameters }) {
-    if (parameters?.nextjs?.navigation) {
-      context.parameters.layout = 'fullscreen'
-    }
-
     context.userEvent = userEvent.setup({
       // When running vitest in browser mode, the pointer events are not correctly simulated.
       // This can be related to this [known issue](https://github.com/microsoft/playwright/issues/12821).
