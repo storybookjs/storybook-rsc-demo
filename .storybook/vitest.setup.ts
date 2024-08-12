179f282b8a4c4fdfcfd72e8fa8b2dd2cab1285fb
Kasper Peulen <kasperpeulen@gmail.com>
2024-08-12 10:22:33 +0200

    Add the a11y addon

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,6 +1,7 @@
 import { beforeAll } from 'vitest'
 import { setProjectAnnotations } from '@storybook/nextjs'
 import * as projectAnnotations from './preview'
+import './a11y.vitest.setup'
 
 const annotations = setProjectAnnotations(projectAnnotations)
 
