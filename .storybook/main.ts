1347978b3f805c1989261e5e89f30b9b1f2742d6
Kasper Peulen <kasperpeulen@gmail.com>
2024-12-03 11:36:30 +0100

    Fix chromatic build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/experimental-nextjs-vite'
+import { mergeConfig } from 'vite'
+import * as path from 'path'
 
 const config: StorybookConfig = {
   stories: ['../docs/**/*.mdx', '../**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -16,5 +18,18 @@ const config: StorybookConfig = {
     experimentalRSC: true,
   },
   staticDirs: ['../public'],
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      // Use a workaround for this prisma vite issue
+      // https://github.com/prisma/prisma/issues/12504#issuecomment-1827097530
+      resolve: {
+        alias: {
+          '.prisma/client/index-browser': require
+            .resolve('@prisma/client/index-browser')
+            .replace(`@prisma${path.sep}client`, `.prisma${path.sep}client`),
+        },
+      },
+    })
+  },
 }
 export default config
