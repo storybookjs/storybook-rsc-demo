d9edc7fc4c42bc27ce2dd0a121059e1c22408c6a
Thomas PEDOT <thomas.pedot@gmail.com>
2025-07-26 12:19:43 +0200

    this commit change the title and will be closed on a PR

--- components/note-editor.stories.tsx
+++ components/note-editor.stories.tsx
@@ -1,10 +1,10 @@
-import { type Meta, type StoryObj } from '@storybook/react'
+import { type Meta, type StoryObj } from '@storybook/react';
 import NoteEditor from "./note-editor";
 
 const meta = {
   component: NoteEditor,
   args: {
-    initialTitle: 'This is a title',
+    initialTitle: 'This title has changed and PR is closed',
     initialBody: 'This is a body',
     noteId: 1,
   }
